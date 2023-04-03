5001d1946583088883c5c63ca3ceeafa42b4e86a
ayushsaranGithuB <112019897+ayushsaranGithuB@users.noreply.github.com>
2023-04-03 14:54:20 +0530

    new branch cloud images

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -1,12 +1,35 @@
 import { buildConfig } from "payload/config";
 import path from "path";
+import { cloudStorage } from '@payloadcms/plugin-cloud-storage';
+import { gcsAdapter } from '@payloadcms/plugin-cloud-storage/gcs';
+
 import Posts from "./collections/Posts";
 import Tags from "./collections/Tags";
 import Users from "./collections/Users";
 import Media from "./collections/Media";
 import Genres from "./collections/Genres";
 
+const adapter = gcsAdapter({
+  options: {
+    // you can choose any method for authentication, and authorization which is being provided by `@google-cloud/storage`
+    keyFilename: './gcs-credentials.json',
+    },
+  bucket: process.env.GCS_BUCKET,
+})
+
+
 export default buildConfig({
+  plugins: [
+    cloudStorage({
+      collections: {
+        'media': {
+          adapter: adapter, // see docs for the adapter you want to use          
+          disablePayloadAccessControl : true,
+        },
+      },
+      
+    }),
+  ],
   serverURL: process.env.PAYLOAD_PUBLIC_SERVER_URL,
   admin: {
     user: Users.slug,
